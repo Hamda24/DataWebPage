3ba7162faffd159ae3d08542d016347202d1932a
Hamda24 <110939252+Hamda24@users.noreply.github.com>
2025-02-19 15:15:47 +0200

    Updated ALLOWED_HOSTS and disabled DEBUG for production

--- Epson Website/EpsonWebsite/EpsonWebsite/settings.py
+++ Epson Website/EpsonWebsite/EpsonWebsite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--0!0k(je25+_=3s%%uvhl&fazvf8(icw)q2#@68sv4&kks8iw!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'yourusername.pythonanywhere.com']
 
 
 # Application definition
